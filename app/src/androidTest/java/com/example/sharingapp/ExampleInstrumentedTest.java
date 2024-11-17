abf2fc16be2a6f053530fa08ad65e6638dd38c5b
Quentin Cartier <daoliangshumaishuji@gmail.com>
2024-11-17 18:18:19 +0100

    Add files

---
+++ app/src/androidTest/java/com/example/sharingapp/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.example.sharingapp;
+
+import android.content.Context;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("com.example.sharingapp", appContext.getPackageName());
+    }
+}
